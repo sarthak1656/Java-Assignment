6f28727f2b4c6e59a42f57d2004a24e4564eeb5c
Sarthak Panigrahi <sipunp70@gmail.com>
2025-03-24 21:05:59 +0530

    Remove Assignment_1.class and update Assignment_1.java to comment out code; add Assignment_2.java for total and average marks calculation

--- src/Assignment_1.java
+++ src/Assignment_1.java
@@ -1,113 +1,113 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Assignment_1 {
-    public static void main(String[] args) {
-        // 1. wap to find out sum,substraction,multiplication and division
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter two numbers: ");
-        int a = s.nextInt();
-        int b = s.nextInt();
-        System.out.println("Addition of "+a+" and "+b+" is: "+(a+b));
-        System.out.println("Substraction of "+a+" and "+b+" is: "+(a-b));
-        System.out.println("Multiplication of "+a+" and "+b+" is: "+(a*b));
-        System.out.println("Division of "+a+" and "+b+" is: "+(a/b));
+// public class Assignment_1 {
+//     public static void main(String[] args) {
+//         // 1. wap to find out sum,substraction,multiplication and division
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter two numbers: ");
+//         int a = s.nextInt();
+//         int b = s.nextInt();
+//         System.out.println("Addition of "+a+" and "+b+" is: "+(a+b));
+//         System.out.println("Substraction of "+a+" and "+b+" is: "+(a-b));
+//         System.out.println("Multiplication of "+a+" and "+b+" is: "+(a*b));
+//         System.out.println("Division of "+a+" and "+b+" is: "+(a/b));
 
-        // ---------------------------------------------------------------------------
+//         // ---------------------------------------------------------------------------
 
-        // 2.wap to print area and perimeter of a circle
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the radius of the circle: ");
-        double r = s.nextDouble();
+//         // 2.wap to print area and perimeter of a circle
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter the radius of the circle: ");
+//         double r = s.nextDouble();
 
-        double area = Math.PI * (r * r);
-        double peri = 2 * Math.PI * r;
-        System.out.println("Area of the circle is: " + area);
-        System.out.println("Perimeter of the circle is: " + peri);
+//         double area = Math.PI * (r * r);
+//         double peri = 2 * Math.PI * r;
+//         System.out.println("Area of the circle is: " + area);
+//         System.out.println("Perimeter of the circle is: " + peri);
 
-        // -----------------------------------------------------------------------
+//         // -----------------------------------------------------------------------
 
-        // 3.wap to print american flag.
-        // System.out.println("* * * * * * ==============================");
-        // System.out.println(" * * * * * ==============================");
-        // System.out.println("* * * * * * ==============================");
-        // System.out.println(" * * * * * ==============================");
-        // System.out.println("* * * * * * ==============================");
-        // System.out.println(" * * * * * ==============================");
-        // System.out.println("* * * * * * ==============================");
-        // System.out.println(" * * * * * ==============================");
-        // System.out.println("* * * * * * ==============================");
-        // System.out.println("==========================================");
-        // System.out.println("==========================================");
-        // System.out.println("==========================================");
-        // System.out.println("==========================================");
-        // System.out.println("==========================================");
-        // System.out.println("==========================================");
+//         // 3.wap to print american flag.
+//         // System.out.println("* * * * * * ==============================");
+//         // System.out.println(" * * * * * ==============================");
+//         // System.out.println("* * * * * * ==============================");
+//         // System.out.println(" * * * * * ==============================");
+//         // System.out.println("* * * * * * ==============================");
+//         // System.out.println(" * * * * * ==============================");
+//         // System.out.println("* * * * * * ==============================");
+//         // System.out.println(" * * * * * ==============================");
+//         // System.out.println("* * * * * * ==============================");
+//         // System.out.println("==========================================");
+//         // System.out.println("==========================================");
+//         // System.out.println("==========================================");
+//         // System.out.println("==========================================");
+//         // System.out.println("==========================================");
+//         // System.out.println("==========================================");
 
-        // ------------------------------------------------------------------------------
+//         // ------------------------------------------------------------------------------
 
-        // 4.wap to swap two numbers.
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter two numbers a and b: ");
-        int a = s.nextInt();
-        int b = s.nextInt();
+//         // 4.wap to swap two numbers.
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter two numbers a and b: ");
+//         int a = s.nextInt();
+//         int b = s.nextInt();
 
-        System.out.println("\nBefore swapping the value of a: " + a);
-        System.out.println("Before swapping the value of b: " + b);
+//         System.out.println("\nBefore swapping the value of a: " + a);
+//         System.out.println("Before swapping the value of b: " + b);
 
-        a = a ^ b;
-        b = a ^ b;
-        a = a ^ b;
+//         a = a ^ b;
+//         b = a ^ b;
+//         a = a ^ b;
 
-        System.out.println("\nAfter swapping the value of a: " + a);
-        System.out.println("After swapping the value of b: " + b);
+//         System.out.println("\nAfter swapping the value of a: " + a);
+//         System.out.println("After swapping the value of b: " + b);
 
-        // --------------------------------------------------------------------
+//         // --------------------------------------------------------------------
 
-        // 5.wap to print smily and sad face
+//         // 5.wap to print smily and sad face
 
-        // System.out.println(" ");
-        // System.out.println(" ** ** ");
-        // System.out.println(" **** **** ");
-        // System.out.println(" ** ** ");
-        // System.out.println(" ");
-        // System.out.println(" ");
-        // System.out.println(" * * ");
-        // System.out.println(" * * ");
-        // System.out.println(" * * ");
-        // System.out.println(" * * ");
-        // System.out.println(" * ");
-        // System.out.println(" ");
+//         // System.out.println(" ");
+//         // System.out.println(" ** ** ");
+//         // System.out.println(" **** **** ");
+//         // System.out.println(" ** ** ");
+//         // System.out.println(" ");
+//         // System.out.println(" ");
+//         // System.out.println(" * * ");
+//         // System.out.println(" * * ");
+//         // System.out.println(" * * ");
+//         // System.out.println(" * * ");
+//         // System.out.println(" * ");
+//         // System.out.println(" ");
 
-        // -------------------------------------------------------------------
+//         // -------------------------------------------------------------------
 
-        // 6. Wap to compare two number.
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter two numbers: ");
-        int a = s.nextInt();
-        int b = s.nextInt();
-        System.out.println(a > b);
-        System.out.println(a < b);
-        System.out.println(a == b);
+//         // 6. Wap to compare two number.
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter two numbers: ");
+//         int a = s.nextInt();
+//         int b = s.nextInt();
+//         System.out.println(a > b);
+//         System.out.println(a < b);
+//         System.out.println(a == b);
 
-        // ----------------------------------------------------------------------
+//         // ----------------------------------------------------------------------
 
-        // 7.wap to compute hexagonal area.
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the length of the side: ");
-        double side = s.nextDouble();
-        double area = ((3 * Math.sqrt(3)) / 2) * (side * side);
+//         // 7.wap to compute hexagonal area.
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter the length of the side: ");
+//         double side = s.nextDouble();
+//         double area = ((3 * Math.sqrt(3)) / 2) * (side * side);
 
-        System.out.println("Area of the hexagone is: " + area);
+//         System.out.println("Area of the hexagone is: " + area);
 
-        // ---------------------------------------------------------------------------
+//         // ---------------------------------------------------------------------------
 
-        // 8.wap to compute the distance between two points on earth surface.
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the two points of the earth: ");
-        double a = s.nextDouble();
-        double b = s.nextDouble();
-        double distance = Math.abs(a-b);
-        System.out.println("The distance between two points are: " + distance);
+//         // 8.wap to compute the distance between two points on earth surface.
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter the two points of the earth: ");
+//         double a = s.nextDouble();
+//         double b = s.nextDouble();
+//         double distance = Math.abs(a-b);
+//         System.out.println("The distance between two points are: " + distance);
 
-    }
-}
+//     }
+// }
--- src/Assignment_2/Assignment_2.java
+++ src/Assignment_2/Assignment_2.java
@@ -1,8 +1,13 @@
+package Assignment_2;
 import java.util.Scanner;
 
 public class Assignment_2 {
 
     public static void main(String[] args) {
+
+        // 1. wap to take a set of numbers from input line as marks of the student,
+        // calculate the total mark and the average mark secured by each student.
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the marks of the student separated by spaces:");
@@ -13,12 +18,15 @@ public class Assignment_2 {
         int numberOfSubjects = marksArray.length;
 
         for (int i = 0; i < numberOfSubjects; i++) {
-            totalMarks += Integer.parseInt(marksArray[i]);
+        totalMarks += Integer.parseInt(marksArray[i]);
         }
 
         double averageMarks = (double) totalMarks / numberOfSubjects;
 
         System.out.println("Total Marks: " + totalMarks);
         System.out.println("Average Marks: " + averageMarks);
+
+        // ---------------------------------------------------------------------------------
+
     }
 }

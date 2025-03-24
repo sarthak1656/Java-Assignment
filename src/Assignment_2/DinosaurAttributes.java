ff8172825e45b31fa275ebb1d1fe65b482912283
Sarthak Panigrahi <sipunp70@gmail.com>
2025-03-24 21:14:39 +0530

    assignment number 3

---
+++ src/Assignment_2/DinosaurAttributes.java
@@ -0,0 +1,18 @@
+// the total distance travelled by a vehicle is given by distance ut+at2 / 2 where u is the initial velocity, a is the acceleration and t is the time. wap to calculate the distance travelled in a regular time given by the value of u and a and time
+
+
+package Assignment_2;
+
+public class DinosaurAttributes {
+    public static void main(String[] args) {
+        double length = 12.5;  // in meters
+        double weight = 5000.75; // in kg
+        double height = 5.8; // in meters
+
+        System.out.println("Dinosaur Attributes:");
+        System.out.println("Length: " + length + " meters");
+        System.out.println("Weight: " + weight + " kg");
+        System.out.println("Height: " + height + " meters");
+    }
+}
+

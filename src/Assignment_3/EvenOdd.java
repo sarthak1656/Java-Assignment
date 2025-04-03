a2457cb4dfcd48121e37d29965eb4b14a1048e7c
Sarthak Panigrahi <sipunp70@gmail.com>
2025-04-03 10:38:20 +0530

    EvenOdd in ass3

---
+++ src/Assignment_3/EvenOdd.java
@@ -0,0 +1,19 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int[] numbers = { 10, 15, 22, 37, 44 };
+
+        System.out.println("Even numbers:");
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                System.out.print(num + " ");
+            }
+        }
+
+        System.out.println("\nOdd numbers:");
+        for (int num : numbers) {
+            if (num % 2 != 0) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}

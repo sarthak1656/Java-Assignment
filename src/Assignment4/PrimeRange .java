833b73e91a989adcff2ed55dc5232aa61fbb8c0e
Sarthak Panigrahi <sipunp70@gmail.com>
2025-04-11 16:44:11 +0530

    Add PrimeRange program and related assignments

---
+++ src/Assignment4/PrimeRange .java
@@ -0,0 +1,29 @@
+package Assignment4;
+
+import java.util.Scanner;
+
+public class PrimeRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the range of prime numbers: ");
+        int n = sc.nextInt();
+        System.out.println("The prime numbers between 1 and " + n + " are: ");
+        for (int i = 2; i <= n; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+
+                }
+
+            }
+            if (!isPrime) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+
+    }
+} 

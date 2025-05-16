a5c6d2de5c28ce19efbe42cb112da4f7f8bcfa0f
Sarthak Panigrahi <sipunp70@gmail.com>
2025-05-16 16:32:07 +0530

    Televison attribute

---
+++ src/Assignment_5/Television.java
@@ -0,0 +1,30 @@
+package Assignment_5;
+public class Television {
+    String make;
+    double screenSize;
+    String dateOfPurchase;
+    boolean isColorTV;
+
+    // Constructor
+    public Television(String make, double screenSize, String dateOfPurchase, boolean isColorTV) {
+        this.make = make;
+        this.screenSize = screenSize;
+        this.dateOfPurchase = dateOfPurchase;
+        this.isColorTV = isColorTV;
+    }
+
+    // Display method
+    public void displayInfo() {
+        System.out.println("Make: " + make);
+        System.out.println("Screen Size: " + screenSize + " inches");
+        System.out.println("Date of Purchase: " + dateOfPurchase);
+        System.out.println("Color TV: " + (isColorTV ? "Yes" : "No"));
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        Television tv = new Television("Sony", 42.5, "2023-08-21", true);
+        tv.displayInfo();
+    }
+}
+

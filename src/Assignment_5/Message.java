a4e86ab34d9694d0f41b9d58350c3ff81d6d2f8e
Sarthak Panigrahi <sipunp70@gmail.com>
2025-06-14 20:42:51 +0530

    overloaded constuctor

--- src/Assignment_5/Message.java
+++ src/Assignment_5/Message.java
@@ -1,13 +1,31 @@
 package Assignment_5;
 
+// public class Message {
+//     Message() {
+//         System.out.println("Default constructor called.");
+//     }
+
+//     public static void main(String[] args) {
+//         Message obj = new Message();
+//     }
+// }
+
+// Default constructor called.
+
 public class Message {
     Message() {
         System.out.println("Default constructor called.");
     }
 
+    Message(String msg) {
+        System.out.println("Message: " + msg);
+    }
+
     public static void main(String[] args) {
-        Message obj = new Message();
+        Message obj1 = new Message();
+        Message obj2 = new Message("Hello from overloaded constructor!");
     }
 }
 
 // Default constructor called.
+// Message: Hello from overloaded constructor!

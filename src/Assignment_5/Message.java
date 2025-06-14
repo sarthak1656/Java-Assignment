9b2b0047a14c6c7a89dc9b21d33dc41477feb5b0
Sarthak Panigrahi <sipunp70@gmail.com>
2025-06-14 20:41:07 +0530

    default constructor

---
+++ src/Assignment_5/Message.java
@@ -0,0 +1,13 @@
+package Assignment_5;
+
+public class Message {
+    Message() {
+        System.out.println("Default constructor called.");
+    }
+
+    public static void main(String[] args) {
+        Message obj = new Message();
+    }
+}
+
+// Default constructor called.
